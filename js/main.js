6afd8614e2f0874e50cc191f7570bc8024021bc6
housemd <housemd@syneforge.com>
2024-10-14 01:08:38 +0300

    Fixed media styles of burger button; Fixed media styles of news-card; Added styles for link into single-news page; Other fixes and improves

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
 function toggleMenu() {
-  const dropdownMenu = document.getElementById("dropdownMenu");
-  const overlay = document.getElementById("overlay");
-  dropdownMenu.classList.toggle("active");
-  overlay.classList.toggle("active");
+  const dropdownMenu = document.getElementById('dropdownMenu');
+  dropdownMenu.classList.toggle('active');
 }
-
 function closeMenu() {
-  const dropdownMenu = document.getElementById("dropdownMenu");
-  const overlay = document.getElementById("overlay");
-  dropdownMenu.classList.remove("active");
-  overlay.classList.remove("active");
+  const dropdownMenu = document.getElementById('dropdownMenu');
+  dropdownMenu.classList.remove('active');
 }
-
-document.getElementById("searchButton").addEventListener("click", function () {
-  const searchIcon = document.getElementById("searchIcon");
-
-  if (this.classList.contains("active")) {
-    searchIcon.src = "img/search.svg";
-    this.classList.remove("active");
-  } else {
-    searchIcon.src = "img/close.svg";
-    this.classList.add("active");
-  }
-});
+// document.getElementById("searchButton").addEventListener("click", function () {
+//   const searchIcon = document.getElementById("searchIcon");
+//
+//   if (this.classList.contains("active")) {
+//     searchIcon.src = "img/search.svg";
+//     this.classList.remove("active");
+//   } else {
+//     searchIcon.src = "img/close.svg";
+//     this.classList.add("active");
+//   }
+// });
