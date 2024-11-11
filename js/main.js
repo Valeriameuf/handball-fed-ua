a103cd93d8c9bdd3cb1cb25d3c159e7c34ca928f
housemd <housemd@syneforge.com>
2024-11-11 05:28:31 +0200

    Improved main menu and related changes;

--- js/main.js
+++ js/main.js
@@ -4,35 +4,15 @@ import MobileMenu from "@js/classes/MobileMenu";
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 
-const bottomMenu = new BottomMenu(
-  "li.header__item i",
-  "fa-chevron-up",
-  "fa-chevron-down",
-  (trigger) => {
-    const targetNode = document.querySelector(
-      trigger.getAttribute("data-target-node-id")
-    );
-    targetNode.classList.toggle("d-block");
-  }
-);
-const submenu = new Submenu(
-  ".submenu-real__menu li > i",
-  "fa-arrow-left",
-  "fa-arrow-right",
-  (trigger) => {
-    const innerMenu = trigger.parentNode.querySelector("ul");
-    innerMenu.classList.toggle("d-inline-block");
-  }
-);
+const bottomMenu = new BottomMenu('#main-header-menu');
+const submenu = new Submenu('#submenu-real');
 
 const mobileMenu = new MobileMenu(
     '#mobile-menu-open',
     '#mobile-menu-close',
-    '#dropdownMenu'
+    '#dropdown-menu'
 )
 
-bottomMenu.init();
-submenu.init();
 mobileMenu.init();
 
 document.addEventListener("DOMContentLoaded", function () {
