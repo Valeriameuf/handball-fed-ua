429ebfc5f82cee7005ae66679de42cb0fc5778a3
housemd <housemd@syneforge.com>
2025-01-20 21:47:48 +0200

    Added hashes to css/js file names

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,88 @@
-const path = require("path");
-const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
+const path = require('path');
+const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
 
 module.exports = {
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
-
-  plugins: [
-    new HtmlBundlerPlugin({
-      entry: {
-        index: {
-          // output dist/index.html
-          import: "./views/index.ejs",
-          data: {
-            isFrontPage: true,
-          },
-        },
-        "single-news": {
-          import: "./views/single-news.ejs",
-        },
-        "general-news": {
-          import: "./views/general-news.ejs",
-        },
-        banner: {
-          import: "./views/banner.ejs",
-        },
-        match: {
-          import: "./views/match.ejs",
-        },
-      },
-      preprocessor: "ejs", // use EJS templating engine
-      // preprocessorOptions: {...},
-      js: {
-        filename: "main.js", // JS output filename
-      },
-      css: {
-        filename: "style.css", // CSS output filename
-      },
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: ["css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(woff2|woff|ttf)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name][ext]",
-        },
-      },
-      {
-        test: /\.(ico|png|jp?g|svg|webp)/,
-        type: "asset/resource",
-        // generator: {
-        //     filename: 'img/[name].[hash:8][ext][query]',
-        // },
-        generator: {
-          // keep original directory structure
-          filename: ({ filename }) => {
-            const srcPath = "img";
-            const regExp = new RegExp(
-              `[\\\\/]?(?:${path.normalize(srcPath)}|node_modules)[\\\\/](.+?)$`
-            );
-            const assetPath = path.dirname(
-              regExp.exec(filename)[1].replace("@", "").replace(/\\/g, "/")
-            );
-
-            return `img/${assetPath}/[name][ext]`;
-          },
-        },
-      },
-    ],
-  },
-
-  resolve: {
-    alias: {
-      "@scss": path.join(__dirname, "/scss/"),
-      "@js": path.join(__dirname, "/js/"),
-      "@img": path.join(__dirname, "/img/"),
-      "@flags": path.join(__dirname, "/node_modules/svg-country-flags/"),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
     },
-  },
+
+    plugins: [
+        new HtmlBundlerPlugin({
+            entry: {
+                index: {
+                    // output dist/index.html
+                    import: './views/index.ejs',
+                    data: {
+                        isFrontPage: true,
+                    },
+                },
+                'single-news': {
+                    import: './views/single-news.ejs',
+                },
+                'general-news': {
+                    import: './views/general-news.ejs',
+                },
+                banner: {
+                    import: './views/banner.ejs',
+                },
+                match: {
+                    import: './views/match.ejs',
+                },
+            },
+            preprocessor: 'ejs', // use EJS templating engine
+            // preprocessorOptions: {...},
+            js: {
+                filename: 'main.[contenthash:8].js', // JS output filename
+            },
+            css: {
+                filename: 'style.[contenthash:8].css', // CSS output filename
+            },
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: ['css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(woff2|woff|ttf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                },
+            },
+            {
+                test: /\.(ico|png|jp?g|svg|webp)/,
+                type: 'asset/resource',
+                // generator: {
+                //     filename: 'img/[name].[hash:8][ext][query]',
+                // },
+                generator: {
+                    // keep original directory structure
+                    filename: ({ filename }) => {
+                        const srcPath = 'img';
+                        const regExp = new RegExp(
+                            `[\\\\/]?(?:${path.normalize(srcPath)}|node_modules)[\\\\/](.+?)$`
+                        );
+                        const assetPath = path.dirname(
+                            regExp.exec(filename)[1].replace('@', '').replace(/\\/g, '/')
+                        );
+
+                        return `img/${assetPath}/[name][ext]`;
+                    },
+                },
+            },
+        ],
+    },
+
+    resolve: {
+        alias: {
+            '@scss': path.join(__dirname, '/scss/'),
+            '@js': path.join(__dirname, '/js/'),
+            '@img': path.join(__dirname, '/img/'),
+            '@flags': path.join(__dirname, '/node_modules/svg-country-flags/'),
+        },
+    },
 };
