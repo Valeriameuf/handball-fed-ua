817d6ecd989a641440167d3a5204154a2f578d17
Valeria Petrushevska <valerevapet@gmail.com>
2025-04-07 21:39:31 +0300

    feat: update HTML and CSS for various pages style: adjust global styles and page-specific designs feat: create new player card section with HTML, CSS, and image

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,59 @@
-const path = require('path');
-const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const path = require("path");
+const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 
 module.exports = {
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
 
     plugins: [
         new HtmlBundlerPlugin({
             entry: {
                 index: {
-                    import: './views/index.ejs',
+                    import: "./views/index.ejs",
                     data: {
                         isFrontPage: true,
                     },
                 },
-                'single-news': {
-                    import: './views/single-news.ejs',
+                "single-news": {
+                    import: "./views/single-news.ejs",
                 },
-                'general-news': {
-                    import: './views/general-news.ejs',
+                "general-news": {
+                    import: "./views/general-news.ejs",
                 },
                 banner: {
-                    import: './views/banner.ejs',
+                    import: "./views/banner.ejs",
                 },
                 match: {
-                    import: './views/match.ejs',
+                    import: "./views/match.ejs",
                 },
                 standings: {
-                    import: './views/standings.ejs',
+                    import: "./views/standings.ejs",
                 },
                 calendar: {
-                    import: './views/calendar.ejs',
+                    import: "./views/calendar.ejs",
+                },
+                "player-card": {
+                    import: "./views/player-card.ejs",
                 },
             },
-            preprocessor: 'ejs',
+            preprocessor: "ejs",
             // preprocessorOptions: {...},
             js: {
-                filename: '[name].[contenthash:8].js',
+                filename: "[name].[contenthash:8].js",
             },
             css: {
-                filename: 'style.[contenthash:8].css',
+                filename: "style.[contenthash:8].css",
             },
         }),
         new WebpackManifestPlugin({
-            fileName: 'manifest.json',
-            filter: (file) => file.path.endsWith('.js') || file.path.endsWith('.css'),
+            fileName: "manifest.json",
+            filter: (file) => file.path.endsWith(".js") || file.path.endsWith(".css"),
             map: (file) => {
-                if (file.name.startsWith('__bundler-plugin-entry__')) {
-                    file.name = file.name.replace('__bundler-plugin-entry__', '');
+                if (file.name.startsWith("__bundler-plugin-entry__")) {
+                    file.name = file.name.replace("__bundler-plugin-entry__", "");
                 }
                 return file;
             },
@@ -62,8 +65,8 @@ module.exports = {
             cacheGroups: {
                 defaultVendors: {
                     test: /[\\/]node_modules[\\/].+\.(js|ts)$/,
-                    name: 'vendors',
-                    chunks: 'all',
+                    name: "vendors",
+                    chunks: "all",
                     priority: -10,
                 },
             },
@@ -74,28 +77,24 @@ module.exports = {
         rules: [
             {
                 test: /\.s?css$/,
-                use: ['css-loader', 'sass-loader'],
+                use: ["css-loader", "sass-loader"],
             },
             {
                 test: /\.(woff2|woff|ttf)$/,
-                type: 'asset/resource',
+                type: "asset/resource",
                 generator: {
-                    filename: 'fonts/[name][ext]',
+                    filename: "fonts/[name][ext]",
                 },
             },
             {
                 test: /\.(ico|png|jp?g|svg|webp)/,
-                type: 'asset/resource',
+                type: "asset/resource",
                 generator: {
                     // keep original directory structure
                     filename: ({ filename }) => {
-                        const srcPath = 'img';
-                        const regExp = new RegExp(
-                            `[\\\\/]?(?:${path.normalize(srcPath)}|node_modules)[\\\\/](.+?)$`
-                        );
-                        const assetPath = path.dirname(
-                            regExp.exec(filename)[1].replace('@', '').replace(/\\/g, '/')
-                        );
+                        const srcPath = "img";
+                        const regExp = new RegExp(`[\\\\/]?(?:${path.normalize(srcPath)}|node_modules)[\\\\/](.+?)$`);
+                        const assetPath = path.dirname(regExp.exec(filename)[1].replace("@", "").replace(/\\/g, "/"));
 
                         return `img/${assetPath}/[name][ext]`;
                     },
@@ -106,10 +105,10 @@ module.exports = {
 
     resolve: {
         alias: {
-            '@scss': path.join(__dirname, '/scss/'),
-            '@js': path.join(__dirname, '/js/'),
-            '@img': path.join(__dirname, '/img/'),
-            '@flags': path.join(__dirname, '/node_modules/svg-country-flags/'),
+            "@scss": path.join(__dirname, "/scss/"),
+            "@js": path.join(__dirname, "/js/"),
+            "@img": path.join(__dirname, "/img/"),
+            "@flags": path.join(__dirname, "/node_modules/svg-country-flags/"),
         },
     },
 };
